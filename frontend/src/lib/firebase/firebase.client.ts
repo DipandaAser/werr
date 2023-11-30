11954a155d929ff317dba029b99cdbb3d176a52f
Aser <aserdipanda@gmail.com>
2023-11-30 17:45:59 +0100

    fix env issue for node adapter

--- frontend/src/lib/firebase/firebase.client.ts
+++ frontend/src/lib/firebase/firebase.client.ts
@@ -3,12 +3,13 @@ import { deleteApp, getApp, getApps, initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth, setPersistence, inMemoryPersistence } from 'firebase/auth'
+import { env } from '$env/dynamic/public';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID
+    apiKey: env.PUBLIC_FIREBASE_API_KEY,
+    authDomain: env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+    appId: env.PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
